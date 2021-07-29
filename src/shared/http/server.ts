0a7b65539f9abd25403bffe82f746d5d45d769da
Lucas <lucaspoletis@gmail.com>
2021-07-29 19:00:21 -0300

    Customizando erros via middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -9,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError){
+
+  }
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
